2606aafd4253ba047ef6a7578f03bd022a5034e8
Vieness <ydvion13@gmail.com>
2022-12-15 13:15:48 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+const initialData = {
+    name: '',
+    surname: '',
+    salary: ''
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [form, setForm] = useState(initialData)
+    const [users, setUsers] = useState([]);
+    const [editableUserData, setEditableUserData] = useState({
+        isEdit: false,
+        index: null
+    })
+    console.log(users);
+    const isNotFieldEmpty = form.name && form.surname && form.salary
+
+    const handleChangeForm = (e) => {
+        setForm({...form, [e.target.name]: e.target.value})
+    }
+    const handleSubmitUser = (e) => {
+        e.preventDefault();
+        if (!isNotFieldEmpty) return
+        if (form?.index) {
+            users.map((user,index) => index === form?.index)
+        } else {
+            setUsers((prev) => [...prev, form]);
+        }
+        setForm(initialData);
+    }
+    const handleEdit = (element, index) => () => {
+        setForm({...element, index})
+    }
+    const handleRemove = () => {
+
+    }
+
+    return (
+        <div>
+            <table>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Surname</th>
+                    <th>Salary</th>
+                    <th>Action</th>
+                </tr>
+                <tbody>
+                {users.map((user, index) => (
+                    <tr>
+                        <td>{index + 1}</td>
+                        <td>{user.name}</td>
+                        <td>{user.surname}</td>
+                        <td>{user.salary}</td>
+                        <td>
+                            <button onClick={handleEdit(user, index)}>edit</button>
+                            <button>remove</button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            <div>
+                <form onSubmit={handleSubmitUser}>
+                    <input value={form.name} name={"name"} onChange={handleChangeForm}/>
+                    <input value={form.surname} name={"surname"} onChange={handleChangeForm}/>
+                    <input value={form.salary} name={"salary"} onChange={handleChangeForm}/>
+                    <div>
+                        <button type={'submit'}>Add</button>
+                        <button type={"reset"}>Cancel</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
